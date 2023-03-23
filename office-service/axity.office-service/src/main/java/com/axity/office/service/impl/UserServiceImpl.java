b88852955abc11f36ea423e1b3b7b6877baf7292
jonathan <jonathan.aldana@axity.com>
2023-03-23 02:18:02 -0600

    Test createUser happypath

--- office-service/axity.office-service/src/main/java/com/axity/office/service/impl/UserServiceImpl.java
+++ office-service/axity.office-service/src/main/java/com/axity/office/service/impl/UserServiceImpl.java
@@ -22,6 +22,8 @@ import com.axity.office.commons.response.GenericResponseDto;
 import com.axity.office.commons.response.PaginatedResponseDto;
 import com.axity.office.model.UserDO;
 import com.axity.office.model.QUserDO;
+import com.axity.office.model.RoleDO;
+import com.axity.office.persistence.RolePersistence;
 import com.axity.office.persistence.UserPersistence;
 import com.axity.office.service.UserService;
 import com.github.dozermapper.core.Mapper;
@@ -40,30 +42,30 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService
-{
+public class UserServiceImpl implements UserService {
   @Autowired
   private UserPersistence userPersistence;
+  @Autowired
+  private RolePersistence rolePersistence;
 
   @Autowired
   private Mapper mapper;
-  
+
   /**
    * {@inheritDoc}
    */
   @Override
-  public PaginatedResponseDto<UserDto> findUsers( PaginatedRequestDto request )
-  {
-    log.debug( "%s", request );
+  public PaginatedResponseDto<UserDto> findUsers(PaginatedRequestDto request) {
+    log.debug("%s", request);
 
     int page = request.getOffset() / request.getLimit();
-    Pageable pageRequest = PageRequest.of( page, request.getLimit(), Sort.by( "id" ) );
+    Pageable pageRequest = PageRequest.of(page, request.getLimit(), Sort.by("id"));
 
-    var paged = this.userPersistence.findAll( pageRequest );
+    var paged = this.userPersistence.findAll(pageRequest);
 
-    var result = new PaginatedResponseDto<UserDto>( page, request.getLimit(), paged.getTotalElements() );
+    var result = new PaginatedResponseDto<UserDto>(page, request.getLimit(), paged.getTotalElements());
 
-    paged.stream().forEach( x -> result.getData().add( this.transform( x ) ) );
+    paged.stream().forEach(x -> result.getData().add(this.transform(x)));
 
     return result;
   }
@@ -72,15 +74,13 @@ public class UserServiceImpl implements UserService
    * {@inheritDoc}
    */
   @Override
-  public GenericResponseDto<UserDto> find( Integer id )
-  {
+  public GenericResponseDto<UserDto> find(Integer id) {
     GenericResponseDto<UserDto> response = null;
 
-    var optional = this.userPersistence.findById( id );
-    if( optional.isPresent() )
-    {
+    var optional = this.userPersistence.findById(id);
+    if (optional.isPresent()) {
       response = new GenericResponseDto<>();
-      response.setBody( this.transform( optional.get() ) );
+      response.setBody(this.transform(optional.get()));
     }
 
     return response;
@@ -90,70 +90,67 @@ public class UserServiceImpl implements UserService
    * {@inheritDoc}
    */
   @Override
-  public GenericResponseDto<UserDto> create( UserDto dto )
-  {
+  public GenericResponseDto<UserDto> create(UserDto dto) {
 
     UserDO entity = new UserDO();
-    this.mapper.map( dto, entity );
+    this.mapper.map(dto, entity);
     entity.setId(null);
 
-    this.userPersistence.save( entity );
+    var roles = new ArrayList<RoleDO>();
+    entity.setRoles(roles);
 
+    dto.getRoles().stream().forEach(r -> {
+      entity.getRoles().add(this.rolePersistence.findById(r.getId()).get());
+    });
+
+    this.userPersistence.save(entity);
     dto.setId(entity.getId());
-
-    return new GenericResponseDto<>( dto );
+    return new GenericResponseDto<>(dto);
   }
 
   /**
    * {@inheritDoc}
    */
   @Override
-  public GenericResponseDto<Boolean> update( UserDto dto )
-  {
-    var optional = this.userPersistence.findById( dto.getId() );
-    if( optional.isEmpty() )
-    {
-      throw new BusinessException( ErrorCode.OFFICE_NOT_FOUND );
+  public GenericResponseDto<Boolean> update(UserDto dto) {
+    var optional = this.userPersistence.findById(dto.getId());
+    if (optional.isEmpty()) {
+      throw new BusinessException(ErrorCode.OFFICE_NOT_FOUND);
     }
 
     var entity = optional.get();
-    
-    
-    entity.setUsername( dto.getUsername() );
-    entity.setEmail( dto.getEmail() );
-    entity.setName( dto.getName() );
-    entity.setLastName( dto.getLastName() );
-    // TODO: Actualizar entity.Roles (?) 
 
-    this.userPersistence.save( entity );
+    entity.setUsername(dto.getUsername());
+    entity.setEmail(dto.getEmail());
+    entity.setName(dto.getName());
+    entity.setLastName(dto.getLastName());
+    // TODO: Actualizar entity.Roles (?)
 
-    return new GenericResponseDto<>( true );
+    this.userPersistence.save(entity);
+
+    return new GenericResponseDto<>(true);
   }
 
   /**
    * {@inheritDoc}
    */
   @Override
-  public GenericResponseDto<Boolean> delete( Integer id )
-  {
-    var optional = this.userPersistence.findById( id );
-    if( optional.isEmpty() )
-    {
-      throw new BusinessException( ErrorCode.OFFICE_NOT_FOUND );
+  public GenericResponseDto<Boolean> delete(Integer id) {
+    var optional = this.userPersistence.findById(id);
+    if (optional.isEmpty()) {
+      throw new BusinessException(ErrorCode.OFFICE_NOT_FOUND);
     }
 
     var entity = optional.get();
-    this.userPersistence.delete( entity );
+    this.userPersistence.delete(entity);
 
-    return new GenericResponseDto<>( true );
+    return new GenericResponseDto<>(true);
   }
 
-  private UserDto transform( UserDO entity )
-  {
+  private UserDto transform(UserDO entity) {
     UserDto dto = null;
-    if( entity != null )
-    {
-      dto = this.mapper.map( entity, UserDto.class );
+    if (entity != null) {
+      dto = this.mapper.map(entity, UserDto.class);
     }
     return dto;
   }
--- office-service/axity.office-service/src/test/java/com/axity/office/service/UserServiceTest.java
+++ office-service/axity.office-service/src/test/java/com/axity/office/service/UserServiceTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.axity.office.commons.dto.RoleDto;
 import com.axity.office.commons.dto.UserDto;
 import com.axity.office.commons.enums.ErrorCode;
 import com.axity.office.commons.exception.BusinessException;
@@ -31,9 +34,8 @@ import com.axity.office.commons.request.PaginatedRequestDto;
  */
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Transactional
-class UserServiceTest
-{
-  private static final Logger LOG = LoggerFactory.getLogger( UserServiceTest.class );
+class UserServiceTest {
+  private static final Logger LOG = LoggerFactory.getLogger(UserServiceTest.class);
 
   @Autowired
   private UserService userService;
@@ -42,18 +44,17 @@ class UserServiceTest
    * Method to validate the paginated search
    */
   @Test
-  void testFindUsers()
-  {
+  void testFindUsers() {
     var request = new PaginatedRequestDto();
-    request.setLimit( 5 );
-    request.setOffset( 0 );
-    var users = this.userService.findUsers( request );
+    request.setLimit(5);
+    request.setOffset(0);
+    var users = this.userService.findUsers(request);
 
-    LOG.info( "Response: {}", users );
+    LOG.info("Response: {}", users);
 
-    assertNotNull( users );
-    assertNotNull( users.getData() );
-    assertFalse( users.getData().isEmpty() );
+    assertNotNull(users);
+    assertNotNull(users.getData());
+    assertFalse(users.getData().isEmpty());
   }
 
   /**
@@ -63,40 +64,57 @@ class UserServiceTest
    */
   @ParameterizedTest
   @ValueSource(ints = { 1 })
-  void testFind( Integer userId )
-  {
-    var user = this.userService.find( userId );
-    assertNotNull( user );
-    LOG.info( "Response: {}", user );
+  void testFind(Integer userId) {
+    var user = this.userService.find(userId);
+    assertNotNull(user);
+    LOG.info("Response: {}", user);
   }
 
   /**
    * Method to validate the search by id inexistent
    */
   @Test
-  void testFind_NotExists()
-  {
-    var user = this.userService.find( 999999 );
-    assertNull( user );
+  void testFind_NotExists() {
+    var user = this.userService.find(999999);
+    assertNull(user);
   }
 
   /**
-   * Test method for
+   * @param id
+   * @return Instance of RoleDto
+   */
+  private RoleDto createRole(int id) {
+    var role = new RoleDto();
+    role.setId(id);
+    return role;
+  }
+
+  /**
+   * Test method for create user happy path
    * {@link com.axity.office.service.impl.UserServiceImpl#create(com.axity.office.commons.dto.UserDto)}.
    */
   @Test
-  @Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
-  void testCreate()
-  {
+  void testCreate() {
+    // Data inicial
+    var list = new ArrayList<RoleDto>();
+    list.add(createRole(1));
+
     var dto = new UserDto();
-    // Crear de acuerdo a la entidad
+    dto.setUsername("JonathanDev");
+    dto.setEmail("jonathan.dev@axity.com");
+    dto.setName("Jonathan");
+    dto.setLastName("Aldana");
+    dto.setRoles(list);
 
-    var response = this.userService.create( dto );
-    assertNotNull( response );
-    assertEquals( 0, response.getHeader().getCode() );
-    assertNotNull( response.getBody() );
+    // Llamada
+    var response = this.userService.create(dto);
 
-    this.userService.delete( dto.getId() );
+    // Validación
+    assertNotNull(response);
+    assertEquals("OK", response.getHeader().getMessage());
+    assertNotNull(response.getBody());
+
+    this.userService.delete(dto.getId());
   }
 
   /**
@@ -104,17 +122,16 @@ class UserServiceTest
    */
   @Test
   @Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
-  void testUpdate()
-  {
-    var user = this.userService.find( 1 ).getBody();
+  void testUpdate() {
+    var user = this.userService.find(1).getBody();
     // TODO: actualizar de acuerdo a la entidad
 
-    var response = this.userService.update( user );
+    var response = this.userService.update(user);
 
-    assertNotNull( response );
-    assertEquals( 0, response.getHeader().getCode() );
-    assertTrue( response.getBody() );
-    user = this.userService.find( 1 ).getBody();
+    assertNotNull(response);
+    assertEquals(0, response.getHeader().getCode());
+    assertTrue(response.getBody());
+    user = this.userService.find(1).getBody();
 
     // Verificar que se actualice el valor
   }
@@ -123,22 +140,21 @@ class UserServiceTest
    * Method to validate an inexistent registry
    */
   @Test
-  void testUpdate_NotFound()
-  {
+  void testUpdate_NotFound() {
     var user = new UserDto();
     user.setId(999999);
-    var ex = assertThrows( BusinessException.class, () -> this.userService.update( user ) );
+    var ex = assertThrows(BusinessException.class, () -> this.userService.update(user));
 
-    assertEquals( ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode() );
+    assertEquals(ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode());
   }
 
   /**
-   * Test method for {@link com.axity.office.service.impl.UserServiceImpl#delete(java.lang.String)}.
+   * Test method for
+   * {@link com.axity.office.service.impl.UserServiceImpl#delete(java.lang.String)}.
    */
   @Test
-  void testDeleteNotFound()
-  {
-    var ex = assertThrows( BusinessException.class, () -> this.userService.delete( 999999 ) );
-    assertEquals( ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode() );
+  void testDeleteNotFound() {
+    var ex = assertThrows(BusinessException.class, () -> this.userService.delete(999999));
+    assertEquals(ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode());
   }
 }
